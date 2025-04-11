e2d947b417b723e1b696bf0cfcc983cd38c8a224
Yncy0 <paalayancy@gmail.com>
2025-04-11 15:25:51 +0800

    finished: layout between login and the main index. Used layouts featured by Nuxt to place the Navigation Bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,29 @@
 <script setup>
-definePageMeta({
-  middleware: ["auth"],
-});
+// definePageMeta({
+//   middleware: ["auth"],
+// });
 
-const supabase = useSupabaseClient();
-const user = useSupabaseUser();
+// const supabase = useSupabaseClient();
+// const user = useSupabaseUser();
 
-watch(
-  user,
-  () => {
-    if (!user.value) return navigateTo("/login");
-  },
-  { immediate: true }
-);
+// watch(
+//   user,
+//   () => {
+//     if (!user.value) return navigateTo("/login");
+//   },
+//   { immediate: true }
+// );
 
-async function handleSignOut() {
-  const { error } = await supabase.auth.signOut();
-  if (error) throw error;
+// async function handleSignOut() {
+//   const { error } = await supabase.auth.signOut();
+//   if (error) throw error;
 
-  navigateTo("/login", { replace: true });
-}
+//   navigateTo("/login", { replace: true });
+// }
 </script>
 
 <template>
   <div>
     <h1>Index</h1>
-    <UButton @click="handleSignOut">Sign Out</UButton>
   </div>
 </template>
